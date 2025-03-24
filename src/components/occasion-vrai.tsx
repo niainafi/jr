197f516c6c0d4deaa0bac54056ed3842b4d832c4
fulicia <niaritriniainafi2@gmail.com>
2025-03-24 06:14:12 -0700

    ariary

--- src/components/occasion-vrai.tsx
+++ src/components/occasion-vrai.tsx
@@ -166,10 +166,12 @@ export default function Occasion() {
                 />
               )}
               <div className="absolute inset-0 bg-accent bg-opacity-90 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center p-4 text-white text-center">
-                <h3 className="text-lg font-bold">{moto.marque} {moto.modèle}</h3>
-                <p className="text-sm mt-2"><strong>Année :</strong> {moto.année}</p>
-                <p><strong>Prix :</strong> {moto.prix} AR</p>
-                <p className="text-sm"><strong>Kilométrage :</strong> {moto.kilométrage} KM</p>
+                <h3 className="text-lg font-bold">{moto.marque} {moto.modele}</h3>
+                <p className="text-sm mt-2"><strong>Année :</strong> {moto.annee}</p>
+                {/*<p><strong>Prix :</strong> {moto.prix} AR</p> */}
+                <p><strong>Prix :</strong> {Number(moto.prix).toLocaleString("fr-MG")} AR</p>
+
+                <p className="text-sm"><strong>Kilométrage :</strong> {moto.kilometrage} KM</p>
                 <p className="text-sm"><strong>Description :</strong> {moto.description}</p>
                 <button
                   onClick={() => openModal(moto)}
