a2ad230e96b1c4181aacd001abbf48bf461abc97
Mendrika <rahelisonhajaina@gmail.com>
2025-03-27 10:18:18 +0300

    fix just rent accueil

--- src/components/just-rent.tsx
+++ src/components/just-rent.tsx
@@ -4,7 +4,7 @@ import JustRentSwipperSlide from './just-rent-swipper'
 import { data } from './carousel-moto/carousel-data'
 export default function JustRent() {
   return (
-    <section className='py-10 lg:py-28'>
+    <section className='py-10 lg:pt-20 pb-32'>
         <h2 className='text-3xl md:text-4xl text-center font-bold'>{'JUST RENT'}</h2>
         <JustRentSwipperSlide images={data.map((item) => ({ src: item.img }))} />
     </section>
