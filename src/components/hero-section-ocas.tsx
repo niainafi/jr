aa9f9937502e7530af433325d2389d462cf85c70
fulicia <niaritriniainafi2@gmail.com>
2025-04-02 02:18:10 -0700

    heroocas

--- src/components/hero-section-ocas.tsx
+++ src/components/hero-section-ocas.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ {/*import React from 'react';
 import Header from './common/layout/header';
 
 export default function HeroSection() {
@@ -17,5 +17,50 @@ export default function HeroSection() {
 
       
     </div>
+  );
+}
+*/}
+
+import React from 'react';
+import Header from './common/layout/header';
+import Image from 'next/image';
+
+export default function HeroSection() {
+  return (
+    <div className="relative h-[380px] md:h-[500px] lg:h-[600px] xl:h-[450px]">
+      {/*
+      <div 
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ 
+          background: "url(/images/occasion/occasion-head.jpg) no-repeat center center/cover" 
+        }}
+        
+      />*/}
+
+
+         <Image 
+              src="/images/occasion/ocas-head-sans-txt.jpg"
+              alt="Background événement"
+              className="absolute inset-0 bg-cover bg-center"
+              layout="fill" 
+              objectFit="cover"
+              priority 
+            />
+      <div className="absolute inset-0 bg-black opacity-50" />
+
+      <Header />
+      <div className='absolute  top-[40%] translate-y-4 left-[65%] -translate-x-[35%] w-[45%] sm:w-[45%] md:w-[45%] lg:w-[50%] flex justify-center'>
+        <HeroTitle />
+      </div>
+      
+
+  
+    </div>
+  );
+}
+function HeroTitle() {
+  return (
+      <img src='/images/typo-flat/occas.png' width={450} height={50} alt='pour les'/>
+    
   );
 }
